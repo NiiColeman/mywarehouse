49da843d5b3f0d7c72b9a3afea5ce005a21f9815
Eric Coleman <nii.cole@outlook.com>
2019-11-07 02:05:28 -0800

    department-user-items

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles', 'items'
+    'django.contrib.staticfiles', 'items', 'departments'
 ]
 
 MIDDLEWARE = [
